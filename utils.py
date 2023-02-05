75f076bb67dfe6765676ca09274d625be59b4d43
shuoli90 <lishuo1@seas.upenn.edu>
2023-02-04 19:05:56 -0500

    update

--- utils.py
+++ utils.py
@@ -1,15 +1,50 @@
 from __future__ import print_function, division
 import os
 import torch
-import pandas as pd
-from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
 from torchvision.transforms import ToTensor
+import random
 
 # Ignore warnings
 import warnings
 warnings.filterwarnings("ignore")
+
+class GluDataset(Dataset):
+    """Glaucoma dataset."""
+
+    def __init__(self, images, labels, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+        
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = self.images[idx]
+        label = self.labels[idx]
+        # print(img_path)
+        image = Image.open(img_path)
+        image_tmp = ToTensor()(image)
+        if image_tmp.shape[0] != 3:
+            return None
+        # sample = {'image': image, 'label': label}
+
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
